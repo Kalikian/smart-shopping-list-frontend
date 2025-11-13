1e8711a734a9531e670cad10cad17f21630c3ffd
Agop Kalikian <hagopk82@outlook.com>
2025-11-13 16:51:37 +0100

    Updates

---
+++ sw.js
@@ -0,0 +1,6 @@
+// public/sw.js
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => self.clients.claim());
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
